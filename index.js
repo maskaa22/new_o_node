a89232fbd956350abafc64218b636cfaa265bcf6
maskaa22 <kyrosika565@gmail.com>
2023-10-05 11:03:05 +0300

    update versel config

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json({extended: true}));
+
 app.use('/api/product', product);
 
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
