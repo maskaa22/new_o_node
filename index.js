044a8a7a300dd1ecb5f3646258af332aa9b1692f
maskaa22 <kyrosika565@gmail.com>
2023-10-05 09:40:04 +0300

    update /api

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const app = exress();
 
 const PORT = process.env.PORT || 5000;
 
-app.use('api/product', product);
+app.use('/api/product', product);
 
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
