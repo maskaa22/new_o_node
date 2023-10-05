5210990ac356cdd7669fe25091efd72c9f90a517
maskaa22 <kyrosika565@gmail.com>
2023-10-05 09:18:13 +0300

    Initial project

---
+++ index.js
@@ -0,0 +1,10 @@
+const exress = require('express');
+const product = require('./routes/product-router');
+
+const app = exress();
+
+const PORT = process.env.PORT || 5000;
+
+app.use('/product', product);
+
+app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
