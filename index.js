44741567bc0f196567401c8cdf1c2b205e2d23a5
maskaa22 <kyrosika565@gmail.com>
2023-10-05 12:24:36 +0300

    versel

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json({extended: true}));
 
-app.use('/api/product', product);
+// app.use('/api/product', product);
+
+app.get('/', (req, res) => res.send('Home Page Route'));
+
+app.get('/about', (req, res) => res.send('About Page Route'));
 
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
